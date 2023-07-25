1cf8717888966efab8318b48f27e7ea28715223c
Joseph Capocci <typicaljoey@Josephs-MacBook-Pro.local>
2023-07-24 20:18:15 -0400

    App:  Implemented react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./Homepage/App";
 import "./global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
