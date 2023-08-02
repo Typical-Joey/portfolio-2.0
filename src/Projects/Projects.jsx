10f661887d1f9de17f5c6e0a8b9ec913de9b48de
Joseph Capocci <typicaljoey@Josephs-MacBook-Pro.local>
2023-08-02 13:40:35 -0500

    App:  Updated app-container to fill screen, updated styling on project cards

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import "./projects.css";
 
 export default function Projects() {
+  const testProjects = [
+    { title: "Test", description: "This is some text", img: "test.png" },
+    { title: "Test", description: "This is some text", img: "test.png" },
+    { title: "Test", description: "This is some text", img: "test.png" },
+    { title: "Test", description: "This is some text", img: "test.png" },
+  ];
+
   return (
     <Container>
-      <div>
-        <h1>This will be a card</h1>
-      </div>
+      <Row>
+        {testProjects.map((project) => (
+          <Col xs={12} sm={12} md={4} lg={4} className="card-col">
+            <Card className="project-card">
+              <h1>{project.title}</h1>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
