0367590ef75cdd8f22c7871e139e476271ed741c
Joseph Capocci <typicaljoey@Josephs-MacBook-Pro.local>
2023-09-05 13:31:49 -0600

    App:  Setup initial projectModal by clicking a button

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
 import placeholderImg from "../images/placeholder-image.png";
+import ProjectModal from "../ProjectModal/projectModal";
 import "./projects.css";
 
 export default function Projects() {
+  const [showModal, setShowModal] = useState(false);
   const testProjects = [
     { title: "Test", description: "This is some text", img: "" },
     { title: "Test", description: "This is some text", img: "" },
@@ -14,22 +16,26 @@ export default function Projects() {
   ];
 
   return (
-    <Container>
-      <Row className="projects-container">
-        {testProjects.map((project) => (
-          <Col xs={12} sm={12} md={4} lg={4} className="card-col">
-            <Card className="project-card">
-              <div className="project-img-overlay">
-                <img
-                  className="project-img"
-                  src={project.img ? project.img : placeholderImg}
-                  alt="didn't work"
-                />
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row className="projects-container">
+          {testProjects.map((project) => (
+            <Col xs={12} sm={12} md={4} lg={4} className="card-col">
+              <Card className="project-card">
+                <div className="project-img-overlay">
+                  <img
+                    className="project-img"
+                    src={project.img ? project.img : placeholderImg}
+                    alt="didn't work"
+                  />
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <button onClick={() => setShowModal(true)}> Show Modal</button>
+      </Container>
+      <ProjectModal show={showModal} onHide={() => setShowModal(false)} />
+    </>
   );
 }
