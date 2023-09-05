e62b705b4ffe59278c0a12d719ea74c48a2fc11c
Joseph Capocci <typicaljoey@Josephs-MacBook-Pro.local>
2023-09-05 12:39:17 -0600

    App: Images are not used from images folder

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
+import placeholderImg from "../images/placeholder-image.png";
 import "./projects.css";
 
 export default function Projects() {
   const testProjects = [
-    { title: "Test", description: "This is some text", img: "test.png" },
-    { title: "Test", description: "This is some text", img: "test.png" },
-    { title: "Test", description: "This is some text", img: "test.png" },
-    { title: "Test", description: "This is some text", img: "test.png" },
+    { title: "Test", description: "This is some text", img: "" },
+    { title: "Test", description: "This is some text", img: "" },
+    { title: "Test", description: "This is some text", img: "" },
+    { title: "Test", description: "This is some text", img: "" },
+    { title: "Test", description: "This is some text", img: "" },
+    { title: "Test", description: "This is some text", img: "" },
   ];
 
   return (
     <Container>
-      <Row>
+      <Row className="projects-container">
         {testProjects.map((project) => (
           <Col xs={12} sm={12} md={4} lg={4} className="card-col">
             <Card className="project-card">
-              <h1>{project.title}</h1>
+              <div className="project-img-overlay">
+                <img
+                  className="project-img"
+                  src={project.img ? project.img : placeholderImg}
+                  alt="didn't work"
+                />
+              </div>
             </Card>
           </Col>
         ))}
