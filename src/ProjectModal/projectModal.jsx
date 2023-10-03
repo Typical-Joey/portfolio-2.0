095d31c483bd5f3c312c9d0f2657ab5926ad54ec
Typical-Joey <jtcapocci@yahoo.com>
2023-10-02 19:37:44 -0500

    App:  Centered items in right modal div

--- src/ProjectModal/projectModal.jsx
+++ src/ProjectModal/projectModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+import "./projectModal.css";
 
 export default function projectModal({ show, project, onHide }) {
   return (
@@ -9,11 +10,15 @@ export default function projectModal({ show, project, onHide }) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      className="modal-container"
     >
-      <Modal.Header closeButton />
-      <Modal.Body>
-        <h4>{project.title}</h4>
-        <p>{project.description}</p>
+      {/* <Modal.Header closeButton /> */}
+      <Modal.Body className="modal-body-container">
+        <div className="left">project-image</div>
+        <div className="right">
+          <h4>{project.title}</h4>
+          <textarea>{project.description}</textarea>
+        </div>
       </Modal.Body>
     </Modal>
   );
